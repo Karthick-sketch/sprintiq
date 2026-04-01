2e7c5a4810374867c9738c9d157a01e9a86db706
Karthick-sketch <karthick246.kh@outlook.com>
2026-04-01 17:41:12 +0000

    SprintIQ backend setup

---
+++ src/test/java/com/karthi/sprintiq/SprintiqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthi.sprintiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
